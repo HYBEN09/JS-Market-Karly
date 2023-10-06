0db7fe288ceaac9ecd357bce1ffd60eda8d54ada
hyebeen byun <hyebeen2658@naver.com>
2023-10-06 23:34:10 +0900

    Setting: eslint & prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:functional/recommended"],
+  plugins: ["functional"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+
+  rules: {
+    "no-var": ["error"],
+    "no-unused-expressions": ["error"],
+    "no-use-before-define": ["error"],
+    "max-depth": [
+      "error",
+      {
+        max: 1,
+      },
+    ],
+    quotes: [
+      "error",
+      "single",
+      {
+        avoidEscape: true,
+      },
+    ],
+  },
+};
