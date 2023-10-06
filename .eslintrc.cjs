581ad58305754f95cdf5750b4176838c0d28d1a0
hyebeen byun <hyebeen2658@naver.com>
2023-10-06 23:50:04 +0900

    Chore: 폴더 정리

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,26 +7,26 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:functional/recommended"],
-  plugins: ["functional"],
+  extends: ['eslint:recommended', 'plugin:functional/recommended'],
+  plugins: ['functional'],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
 
   rules: {
-    "no-var": ["error"],
-    "no-unused-expressions": ["error"],
-    "no-use-before-define": ["error"],
-    "max-depth": [
-      "error",
+    'no-var': ['error'],
+    'no-unused-expressions': ['error'],
+    'no-use-before-define': ['error'],
+    'max-depth': [
+      'error',
       {
         max: 1,
       },
     ],
     quotes: [
-      "error",
-      "single",
+      'error',
+      'single',
       {
         avoidEscape: true,
       },
