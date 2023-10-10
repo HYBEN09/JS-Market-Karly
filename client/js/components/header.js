58f36cc662a6d64860878a7b1131ad92575aa7d5
hyebeen byun <hyebeen2658@naver.com>
2023-10-10 17:26:07 +0900

    Feat: 배너 숨기는 기능 구현

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -1,5 +1,22 @@
+import { getNode } from '../../lib/index.js';
+
+/* ------------------------------- 배너를 숨기는 함수 ------------------------------- */
+const closeBannerButton = getNode('.top-banner__close-btn');
+const topBanner = getNode('.top-banner');
+
+function hideBanner() {
+  topBanner.style.display = 'none';
+}
+
+function setupCloseButton() {
+  closeBannerButton.addEventListener('click', hideBanner);
+}
+
+document.addEventListener('DOMContentLoaded', setupCloseButton);
+
+/* ------------------------------- 공지사항 토글 메뉴 ------------------------------- */
 const menuToggle = document.getElementById('menu-toggle');
-const dropdownMenu = document.querySelector('.header__dropdown.menu');
+const dropdownMenu = getNode('.header__dropdown.menu');
 
 dropdownMenu.addEventListener('mouseleave', () => {
   dropdownMenu.style.display = 'none';
