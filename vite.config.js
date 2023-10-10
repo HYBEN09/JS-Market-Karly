a9a521e64ab47ac311d072e44132d4538cebad34
hyebeen byun <hyebeen2658@naver.com>
2023-10-11 01:57:30 +0900

    Feat: vite.config.js수정

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-export default {
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
   root: 'client',
+  build: {
+    outDir: path.resolve(__dirname, '../dist'),
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'client/index.html'),
+        login: path.resolve(__dirname, 'client/pages/login.html'),
+        register: path.resolve(__dirname, 'client/pages/register.html'),
+      },
+    },
+  },
   server: {
     host: 'localhost',
     port: 3000,
   },
-  build: {
-    outDir: '../dist',
-  },
   esbuild: {
     target: 'es2022',
   },
-};
+});
