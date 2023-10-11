25682cb6525b6c7a392f442db7fccb944a25b3ef
hyebeen byun <hyebeen2658@naver.com>
2023-10-11 11:13:26 +0900

    Feat: 메뉴 클릭 토글 함수 구현

---
+++ client/js/pages/productList.js
@@ -0,0 +1,36 @@
+import { getNode, getNodes } from '../../lib/index.js';
+
+// DOM 요소 선택
+const listItems = getNodes('.best-list li');
+const sortOptions = getNodes('.sort-option');
+const toggleButtons = getNodes('.toggle-btn');
+
+// 메뉴 클릭 토글 함수
+function toggleListNav(e) {
+  const listItem = e.currentTarget.parentElement;
+  const listNav = listItem.nextElementSibling;
+
+  listNav.classList.toggle('active');
+  listItem.classList.toggle('active');
+  e.currentTarget.classList.toggle('active');
+}
+
+// 메뉴 클릭 토글 함수를 버튼에 각각 추가
+function toggleNavActive(item, otherItems) {
+  otherItems.forEach((otherItem) => {
+    if (otherItem !== item) {
+      otherItem.classList.remove('nav-active');
+    }
+  });
+
+  item.classList.toggle('nav-active');
+}
+
+function initializeToggleButtons() {
+  toggleButtons.forEach((button) => {
+    button.addEventListener('click', toggleListNav);
+  });
+}
+
+// ---------------------------------------------------------------
+initializeToggleButtons();
