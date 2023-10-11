dc35c1345070b4cdd7878f1e6cace74058ba16f5
hyebeen byun <hyebeen2658@naver.com>
2023-10-11 22:49:00 +0900

    Feat: 동적으로 상품목록 구현 - json-server 설치

--- client/js/pages/productList.js
+++ client/js/pages/productList.js
@@ -5,6 +5,7 @@ const listItems = getNodes('.best-list li');
 const sortOptions = getNodes('.sort-option');
 const toggleButtons = getNodes('.toggle-btn');
 const productList = getNode('.product-list_nav');
+const bestContainer = getNode('.best-container');
 
 /* -------------------------------------------------------------------------- */
 /*                             메뉴 클릭 토글 함수                                */
@@ -16,17 +17,6 @@ function toggleListNav(e) {
   listNav.classList.toggle('active');
   listItem.classList.toggle('active');
   e.currentTarget.classList.toggle('active');
-}
-
-// 메뉴 클릭 토글 함수를 버튼에 각각 추가
-function toggleNavActive(item, otherItems) {
-  otherItems.forEach((otherItem) => {
-    if (otherItem !== item) {
-      otherItem.classList.remove('nav-active');
-    }
-  });
-
-  item.classList.toggle('nav-active');
 }
 
 function initializeToggleButtons() {
@@ -48,6 +38,86 @@ function activateSortOption() {
   });
 }
 
+/* -------------------------------------------------------------------------- */
+/*                Product 데이터를 불러오고 화면에 보여주는 함수                        */
+/* -------------------------------------------------------------------------- */
+function generateDiscountInfo(discountRate) {
+  return `
+    <dd class="best__discount-rate">${(discountRate * 100).toFixed(0)}%</dd>
+  `;
+}
+// 상품 HTML을 생성하는 함수
+function generateProductHTML(product) {
+  const discountInfo = product.saleRatio
+    ? generateDiscountInfo(product.saleRatio)
+    : '';
+
+  const priceInfo = product.saleRatio
+    ? `
+    <dd class="best__price"> ${Math.floor(
+      product.price * (1 - product.saleRatio)
+    )}원</dd>
+    <dd class="best__price"> ${Math.floor(
+      product.price * (1 - product.saleRatio)
+    )}원</dd>`
+    : `<dd class="best__price">${product.price}원</dd>`;
+
+  return /*html*/ `
+    <a href="#">
+      <figure class="best-img_wrapper">
+        <div class="image-container">
+          <img src="${product.image.thumbnail}" alt="${
+            product.image.alt
+          }" class="best-product_img" />
+        </div>
+        <button class="best-cart_btn">
+          <img src="../../assets/icons/cart.png" alt="장바구니 아이콘" class="best-cart_img" />
+        </button>
+        <figcaption class="best-img_container">
+          <h3 class="a11yHidden">${product.name}</h3>
+          <dl class="best-item">
+            <dt aria-hidden="true" class="a11yHidden">배송안내</dt>
+            <dd class="best__delivery">샛별배송</dd>
+            <dt aria-hidden="true" class="a11yHidden">배송안내</dt>
+            <dd id="${product.id}" class="best__name">${product.name}</dd>
+            <div class="price_info">
+              <dt class="a11yHidden">가격</dt>
+              ${priceInfo}
+              <dt class="a11yHidden">상품설명</dt>
+            </div>
+            <dd class="best__info">${product.description}</dd>
+        ${
+          product.kalryOnly === 'true'
+            ? `
+            <div class="best-message">
+              <dt aria-hidden="true" class="a11yHidden">단독</dt>
+              <dd class="best__only">Karly Only</dd>
+              <dt aria-hidden="true" class="a11yHidden">수량정보</dt>
+              <dd class="best__limit">한정수량</dd>
+            </div>
+            `
+            : `<div style="height: 34px;"></div>`
+        }
+          </dl>
+        </figcaption>
+      </figure>
+    </a>`;
+}
+
+// 상품 목록을 로드하고 표시하는 함수
+function loadAndDisplayProducts() {
+  fetch('http://localhost:5000/products')
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((product) => {
+        const productHTML = generateProductHTML(product);
+        bestContainer.innerHTML += productHTML;
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
 /* ---------------------------------- 함수 실행 ---------------------------- */
 initializeToggleButtons();
 activateSortOption();
+loadAndDisplayProducts();
